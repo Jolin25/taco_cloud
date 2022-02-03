a05eb71ca1bd67d09b82177aa78f1875684e880a
JolyPhone <18990341730@163.com>
2022-02-03 22:48:32 +0800

    spring security ： 自定义用户认证 不同路径的访问权限设置

--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -91,4 +91,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .passwordEncoder(encoder());
 
     }
+
+    /**
+     * 用于配置需要拦截的请求，以及对应的如需访问用户需要具备的条件
+     *
+     * @param
+     * @return
+     * @date 2022/2/3
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // 配置的顺序很重要，前面的优先级比后面高
+        http
+                .authorizeRequests()
+                .antMatchers("/design", "/orders")
+                .hasRole("ROLE_USER")
+                .antMatchers("/", "/**").permitAll();
+    }
 }
