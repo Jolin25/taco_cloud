46a370076e0ce8ffe6238e8a39bc13bdb59f403f
jrl <584189010@qq.com>
2022-02-15 11:22:43 +0800

    加注释

--- src/main/java/tacos/data/OrderRepository.java
+++ src/main/java/tacos/data/OrderRepository.java
@@ -3,8 +3,11 @@ package tacos.data;
 import org.springframework.data.repository.CrudRepository;
 import tacos.po.Order;
 
-
-public interface OrderRepository 
-         extends CrudRepository<Order, Long> {
+/**
+ * @author jrl
+ * @date 2022-2-15
+ */
+public interface OrderRepository
+        extends CrudRepository<Order, Long> {
 
 }
